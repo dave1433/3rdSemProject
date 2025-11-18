6fcee6b485d74b124d3cca1fe92fb4c0bf06e998
David Ch <raulchuchuca951@gmail.com>
2025-11-18 11:14:21 +0100

    .gitignore updated + succeded scaffold

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,37 @@
+using api;
+using efscaffold.Entities;
+using Infrastructure.Postgres.Scaffolding;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var appSettings = builder.Services.AddAppSettings(builder.Configuration);
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpgsql(appSettings.DefaultConnection);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (
+    
+    [FromServices]IOptionsMonitor<AppSettings> optionsMonitor, 
+    [FromServices] MyDbContext dbContext) =>
+{
+    var newPlayer = new Player()
+    {
+        Id = Guid.NewGuid().ToString(),
+        Fullname = "John Doe",
+        Active = true,
+        Balance = 100
+    };
+    dbContext.Players.Add(newPlayer);
+    dbContext.SaveChanges();
+    var objects = dbContext.Players.ToList();
+    return objects;
+});
 
 app.Run();
