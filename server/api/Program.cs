c4cb533e628538f24957a647b767f37bf413ca1a
Alex <alx_sp@yahoo.com>
2025-12-10 18:41:49 +0200

    board scaffold

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -108,7 +108,7 @@ builder.Services.AddCors(options =>
     {
         policy
             .WithOrigins(
-                "http://localhost:5173",
+                "http://localhost:5174",
                 "https://deadpigeons-frontend.fly.dev"
             )
             .AllowAnyHeader()
